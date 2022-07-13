c0348dc7755e1edcf79ba9e7f86cac830448ce2b
Syed Sayeeduddin <iamsayeeduddin@outlook.com>
2022-07-14 01:35:43 +0530

    API URL UPDATED

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-const base = "http://122.175.33.146:7070/api";
+const base = "https://tcqapp.theclearquran.org/";
 export const routes = {
-    getChapters: `${base}/GetChapters`,
-    registerUser: `${base}/RegisterUser`,
-    updateUser: `${base}/ConfirmUser`,
-    createNote: `${base}/CreateNote`,
-    getAllNotes: `${base}/GetAllNotes`,
-    updateNote: `${base}/UpdateNote`,
-    addBookmark: `${base}/Bookmark`,
-    contactUs: `${base}/AddContactUs`,
-    getAboutUs: `${base}/GetAboutUs`
-}
+  getChapters: `${base}/GetChapters`,
+  registerUser: `${base}/RegisterUser`,
+  updateUser: `${base}/ConfirmUser`,
+  createNote: `${base}/CreateNote`,
+  getAllNotes: `${base}/GetAllNotes`,
+  updateNote: `${base}/UpdateNote`,
+  addBookmark: `${base}/Bookmark`,
+  contactUs: `${base}/AddContactUs`,
+  getAboutUs: `${base}/GetAboutUs`,
+};
 export const headers = {
   "Content-Security-Policy": 1,
 };
